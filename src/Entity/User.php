8edc2d0fbab6d530d0df6964068fb1ea91272f7c
tomaszkedziora14 <tomaszkedziora14@wp.pl>
2023-04-18 22:42:49 +0200

    Update User.php

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -72,89 +72,89 @@ class User extends BaseEntity implements
 	protected $apiToken;
 
 	public function getName(): ?string
-    {
+    	{
 		return $this->name;
 	}
 
 	public function setName(string $name): self
-    {
+    	{
 		$this->name = $name;
 		return $this;
 	}
 
 	public function getEmail(): ?string
-    {
+    	{
 		return $this->email;
 	}
 
 	public function setEmail(string $email): self
-    {
+    	{
 		$this->email = $email;
 		return $this;
 	}
 
 	public function getPassword(): ?string
-    {
+    	{
 		return $this->password;
 	}
 
 	public function setPassword(string $password): self
-    {
+    	{
 		$this->password = $password;
 		return $this;
 	}
 
 	public function getRoles(): ?array
-    {
+    	{
 		$roles = $this->roles;
 		$roles[] = 'ROLE_USER';
 		return array_unique($roles);
 	}
 
 	public function setRoles(array $roles): self
-    {
+    	{
 		$this->roles = $roles;
 		return $this;
 	}
 
 	public function getConfirmationStatus(): bool
-    {
+    	{
 		return $this->confirmationStatus;
 	}
 
 	public function setConfirmationStatus(bool $confirmationStatus): self
-    {
+    	{
 		$this->confirmationStatus = $confirmationStatus;
 		return $this;
 	}
 
 	public function getGravatar(): ?string
-    {
+    	{
 		return $this->gravatar;
 	}
 
 	public function getApiToken(): string
-    {
+    	{
 		return $this->apiToken;
 	}
 
 	public function setApiToken(string $apiToken): self
-    {
+   	 {
 		$this->apiToken = $apiToken;
 		return $this;
 	}
 
 	public function getUserIdentifier()
-    {
+    	{
 		return $this->email;
 	}
 
 	public function getSalt()
-    {
+    	{
 	}
 
 	public function getUsername()
-    {
+   	{
 		return $this->getUserIdentifier();
 	}
 
